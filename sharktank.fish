3a6976224ca6f1b1388e6685796c8d0f2731e458
goodcodeguy <gene.shepherd@gmail.com>
2013-11-22 16:29:14 -0500

    The reorganization is complete and all is well again in sharktank land

--- sharktank.fish
+++ sharktank.fish
@@ -1,14 +1,14 @@
 # Load environment scripts
-for load_file in $fish_path/load/*/*.load
+for load_file in $fish_path/tank/*/load/*.load
 	. $load_file
 end
 
 # Load function scripts
-for function_path in $fish_path/functions/*
+for function_path in $fish_path/tank/*/functions
   set fish_function_path $function_path $fish_function_path
 end
 
 # Load Completions
-for complete_path in $fish_path/completions/*
+for complete_path in $fish_path/tank/*/completions
   set fish_complete_path $complete_path $fish_complete_path
 end
