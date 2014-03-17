927b870164388c5242bc6db4268a4881799c3dcf
goodcodeguy <gene.shepherd@gmail.com>
2014-03-17 11:08:56 -0400

    trivial changes

--- sharktank.fish
+++ sharktank.fish
@@ -1,11 +1,11 @@
-# Load environment scripts
-for load_file in $fish_path/tank/*/load/*.load
-	. $load_file
-end
-
 # Load function scripts
 for function_path in $fish_path/tank/*/functions
   set fish_function_path $function_path $fish_function_path
+end
+
+# Load environment scripts
+for load_file in $fish_path/tank/*/load/*.load
+	. $load_file
 end
 
 # Load Completions
